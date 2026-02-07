1a53f09e796bcc475d354e507fb908c769f3fe40
Nikita Fenyak <nefenyak92@yandex.ru>
2026-02-08 01:25:39 +0300

    refactirong src

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -13,8 +13,6 @@ export function initFiltering(elements) {
   }
 
   const applyFiltering = (query, state, action) => {
-    // код с обработкой очистки поля
-    // @todo: #4.2 — обработать очистку поля
     if (action && action.name === 'clear') {
       const field = action.dataset.field
       const container = action.parentElement
@@ -24,28 +22,26 @@ export function initFiltering(elements) {
         input.value = ''
         state[field] = ''
       }
-      // @todo: #4.5 — отфильтровать данные, используя компаратор
-      const filter = {}
-      Object.keys(elements).forEach((key) => {
-        if (elements[key]) {
-          if (
-            ['INPUT', 'SELECT'].includes(elements[key].tagName) &&
-            elements[key].value
-          ) {
-            // ищем поля ввода в фильтре с непустыми данными
-            filter[`filter[${elements[key].name}]`] = elements[key].value // чтобы сформировать в query вложенный объект фильтра
-          }
-        }
-      })
-
-      return Object.keys(filter).length
-        ? Object.assign({}, query, filter)
-        : query // если в фильтре что-то добавилось, применим к запросу
     }
 
-    return {
-      updateIndexes,
-      applyFiltering,
-    }
+    // @todo: #4.5 — отфильтровать данные, используя компаратор
+    const filter = {}
+
+    Object.keys(elements).forEach((key) => {
+      if (
+        elements[key] &&
+        ['INPUT', 'SELECT'].includes(elements[key].tagName) &&
+        elements[key].value
+      ) {
+        filter[`filter[${elements[key].name}]`] = elements[key].value
+      }
+    })
+
+    return Object.keys(filter).length ? Object.assign({}, query, filter) : query
+  }
+
+  return {
+    applyFiltering,
+    updateIndexes,
   }
 }
